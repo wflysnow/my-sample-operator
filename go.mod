36b24fbfb0abf9762c583938d22f74fed7e88278
snow <snow@mail.com>
2020-11-23 17:08:58 +0800

    update dependencies

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	k8s.io/apimachinery v0.0.0-20190425132440-17f84483f500
-	k8s.io/client-go v0.0.0-20190425172711-65184652c889 // indirect
+	k8s.io/client-go v0.0.0-20190425172711-65184652c889
 	k8s.io/code-generator v0.0.0-20190419212335-ff26e7842f9d
 )
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"my-sample-operator/pkg/generated/clientset/versioned"
+	"my-sample-operator/pkg/generated/clientset/versioned/typed/samplecontroller/v1alpha1"
+	"my-sample-operator/pkg/generated/informers/externalversions"
+	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func TestClient(t *testing.T) {
+	config, err := clientcmd.BuildConfigFromFlags("192.168.15.228:6443", "E:/config/config")
+	if err != nil {
+		panic(err.Error())
+	}
+	client, err := v1alpha1.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	fooList, err := client.Foos("test").List(metav1.ListOptions{})
+	fmt.Println(fooList, err)
+
+	clientset, err := versioned.NewForConfig(config)
+	factory := externalversions.NewSharedInformerFactory(clientset, 30*time.Second)
+	foo, err := factory.Samplecontroller().V1alpha1().Foos().Lister().Foos("test").Get("test")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(foo, err)
+
+}
